f045bdbafac78aa91035af86e0dbfd10de368b58
glorphindale <glorphindale@gmail.com>
2020-06-17 13:51:32 +0700

    stage1: Draw sprites instead of boxes.

--- js/game.js
+++ js/game.js
@@ -6,4 +6,18 @@ function setupCanvas() {
     canvas.style.height = canvas.height + 'px';
 
     ctx = canvas.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
+}
+
+function draw_sprite(sprite, x, y) {
+    ctx.drawImage(
+        spritesheet,
+        sprite*16, 0, 16, 16,
+        x*tile_size, y*tile_size, tile_size, tile_size
+    );
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    draw_sprite(0, x, y);
 }
